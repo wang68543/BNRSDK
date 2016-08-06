b9ea15b2cf81ab60d38e954bdc22a9624f049cb3
JustinYang <ityangjing@gmail.com>
2016-08-06 18:00:33 +0800

    new BNRSDKLib 0.0.1

---
+++ BNRSDKLib/BNRSDKLib/BNRSDKLib.h
@@ -0,0 +1,20 @@
+//
+//  BNRSDKLib.h
+//  BNRSDKLib
+//
+//  Created by JustinYang on 8/6/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BNRSDKLib.
+FOUNDATION_EXPORT double BNRSDKLibVersionNumber;
+
+//! Project version string for BNRSDKLib.
+FOUNDATION_EXPORT const unsigned char BNRSDKLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BNRSDKLib/PublicHeader.h>
+
+
+#import <BNRSDKLib/AirPlayDeviceAutoSelect.h>
