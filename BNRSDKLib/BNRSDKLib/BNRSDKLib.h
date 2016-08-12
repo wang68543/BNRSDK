09783bb49e623e1677acbcdfcd42e1fac2779917
JustinYang <ityangjing@gmail.com>
2016-08-12 10:48:54 +0800

    for update pod specs

--- BNRSDKLib/BNRSDKLib/BNRSDKLib.h
+++ BNRSDKLib/BNRSDKLib/BNRSDKLib.h
@@ -16,7 +16,7 @@ FOUNDATION_EXPORT const unsigned char BNRSDKLibVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BNRSDKLib/PublicHeader.h>
 
-
+#import <BNRSDKLib/BNRMacro.h>
 #import <BNRSDKLib/AirPlayDeviceAutoSelect.h>
 #import <BNRSDKLib/NSDate+SSToolkitAdditions.h>
 #import <BNRSDKLib/NSNotification+BNRAddditions.h>
